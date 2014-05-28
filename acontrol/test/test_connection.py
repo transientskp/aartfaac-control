72d46e031e374386bd484aa15116a856a6f10d37
Error323 <folkerthuizinga@gmail.com>
2014-05-28 16:57:40 +0200

    fix tests

--- acontrol/test/test_connection.py
+++ acontrol/test/test_connection.py
@@ -2,20 +2,21 @@ import unittest
 
 from acontrol.connection import Connection
 
+CMD = "ls"
+
 class ConnectionTestCase(unittest.TestCase):
-  CMD = "ls"
   def setUp(self):
     self.conn = Connection('localhost')
 
   def tearDown(self):
-    for v in self.conn.itervalues():
+    for v in self.conn.channels.items():
       self.conn.stop_program(v)
 
   def test_cmd(self):
-    self.conn.start_program(cmd)
-    self.assertTrue(self.conn.channels.has_key(cmd))
-    self.conn.stop_program(cmd)
-    self.assertFalse(self.conn.channels.has_key(cmd))
+    self.conn.start_program(CMD)
+    self.assertTrue(self.conn.channels.has_key(CMD))
+    self.conn.stop_program(CMD)
+    self.assertFalse(self.conn.channels.has_key(CMD))
 
 
 if __name__ == "__main__":
