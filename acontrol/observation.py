ec970c5742dbf1befab0c16410a759e91df4a882
Error323 <folkerthuizinga@gmail.com>
2014-10-09 14:41:39 +0000

    improve observation representation

--- acontrol/observation.py
+++ acontrol/observation.py
@@ -4,9 +4,10 @@ class Observation(object):
     """
     Encapsulates a LOFAR observation.
     """
-    def __init__(self, parset):
-        self.parset = parset
-        p = Parset(self.parset)
+    def __init__(self, filepath):
+        self.parset = filepath
+        self.filepath = filepath
+        p = Parset(self.filepath)
         self.start_time = p.getDateTime("Observation.startTime")
         self.end_time = p.getDateTime("Observation.stopTime")
         self.antenna_array = p.getString("Observation.antennaArray")
@@ -17,8 +18,8 @@ class Observation(object):
         self.start_freq = self.start_subband*self.sample_clock*1e6/1024.0
         self.chan_width = self.sample_clock*1e6/(64.0*1024.0)
 
-    def save(self, filename):
-      self.parset.writeFile(filename)
+    def save(self, filepath):
+      self.parset.writeFile(filepath)
 
     @property
     def duration(self):
@@ -33,7 +34,7 @@ class Observation(object):
         return self.valid and self.antenna_array == "LBA"
 
     def __str__(self):
-        return "OBS - A:%s F:%f W:%f (%s   %s)" % (self.antenna_set, self.start_freq, self.chan_width, self.start_time, self.end_time)
+        return "OBS - %s %0.1fMHz [%s]" % (self.antenna_set, self.start_freq*1e-6, self.filepath)
 
     def __cmp__(self, other):
         return cmp(self.start_time, other.start_time)
