ecd440624e4c5f5b3e19b8d1eacf184a55bca953
Peeyush Prasad <hsuyeep@gmail.com>
2015-01-30 14:14:00 +0000

    Working cmdclient, with subclassing for different processing blocks. Added commandline parameters for specifying the processing block.

--- acontrol/clients/cmdclient.py
+++ acontrol/clients/cmdclient.py
@@ -12,45 +12,103 @@
 import sys;
 import socket;
 import thread;
+import subprocess;
 import time;
+import argparse;
 
 class cmdClient:
-	_cmdport = 44000; # Port on which command server should connect.
+	_cmdport = 45000; # Port on which command server should connect.
 
 	def __init__ (self):
 		self._cmd = 'READY';
 		self._fid = socket.socket (socket.AF_INET, socket.SOCK_STREAM);
 		print '--> Binding on port ', self._cmdport;
 		self._fid.bind( ('', self._cmdport) );
+		self._runcmd = ['date'];
 
 	def run (self):
 		print 'In run'
-		while (self._cmd is not 'QUIT'):
+		while (self._cmd != 'QUIT'):
 			self._fid.listen(1); # Blocking wait
 			self._servsock, self._servaddr = self._fid.accept();
-			print '--> Received connection from ', self._servaddr;
-			thread.start_new_thread (threadhdlr, (self._servsock, self._servaddr));
+			print '--> Received connection from ', self._servaddr, 'cmd', self._cmd, self._cmd != 'QUIT', type (self._cmd);
+			thread.start_new_thread (self.threadhdlr,());
 
 	def threadhdlr (self):
-		line = self._servsock.recv (1024);
-		print 'Client sent:' ,line;
+		self._cmd = self._servsock.recv (1024);
 		self._status = 'OK';
+		if self._cmd is 'START':
+			try:
+				cmdout = subprocess.check_call (self._runcmd, shell=True);
+			except CalledProcessError:
+				print 'Error in executing process!';
+				self._status = 'NOK';
+
+		elif self._cmd is 'STOP':
+#			try:
+#				cmdout = subprocess.check_call (self._runcmd, shell=True);
+#			except CalledProcessError:
+#				print 'Error in executing process!';
+			self._status = 'OK';
+
 		print 'We send:', self._status;
 		self._servsock.send(self._status);
 
+	# Method to show the history of commands received by this cmdclient
+	# TODO
+	def showcmdhist (self):
+		return False;
+
 	def __del__(self):
 		# Send termination signal to all threads
+		# TODO.
 		self._fid.close();	
 	
 
-# Object to handle teh startup of a pelican server, including generating a 
+# Object to handle the startup of a pelican server, including generating a 
 # status on whether startup was successful, based on parsing the stdout.
 class pelicanServerCmdClient (cmdClient):
-
 	def __init__ (self):
 		cmdClient.__init__(self);
+		# self._runcmd = ['ls -l'];
+
+class pipelineCmdClient (cmdClient):
+	def __init__ (self):
+		cmdClient.__init__(self);
+		# self._runcmd = ['ls -l'];
+
+class gpuCorrCmdClient (cmdClient):
+	def __init__ (self):
+		cmdClient.__init__(self);
+		# self._runcmd = ['ls -l'];
+
+
+class lcuafaacCmdClient (cmdClient):
+	def __init__ (self):
+		cmdClient.__init__(self);
+		# self._runcmd = ['ls -l'];
+
 
 if __name__ == '__main__':
-	cl = cmdClient();
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--procblk", help="Specify which processing block cmdclient controls", default='gpucorr');
+	client = parser.parse_args();
+
+	if client.procblk == 'gpucorr':
+		cl = gpuCorrCmdClient ();
+
+	elif client.procblk == 'lcu':
+		cl = lcuafaacCmdClient();	
+
+	elif client.procblk == 'pipe':
+		cl = pipelineCmdClient();
+
+	elif client.procblk == 'pelican':
+		cl = pelicanserverCmdClient();
+
+	else:
+		print '### Unknown processing block!';
+		sys.exit (-1);
+
 	cl.run();	
 
